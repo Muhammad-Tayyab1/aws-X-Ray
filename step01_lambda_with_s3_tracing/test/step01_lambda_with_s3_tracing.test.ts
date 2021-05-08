e419539d6e2b6fd3772bcd5d16b84ffc03d20546
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-05-08 12:36:21 +0500

    Initial commit

---
+++ step01_lambda_with_s3_tracing/test/step01_lambda_with_s3_tracing.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Step01LambdaWithS3Tracing from '../lib/step01_lambda_with_s3_tracing-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Step01LambdaWithS3Tracing.Step01LambdaWithS3TracingStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
